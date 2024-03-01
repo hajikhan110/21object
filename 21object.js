16e6e396b9d84fe8ffef003bd1dad4d2125ed90b
hajikhan <hajikhan110@yahoo.com>
2024-03-01 16:00:37 +0500

    msg

---
+++ 21object.js
@@ -0,0 +1,6 @@
+//regarding object
+var country = {
+    yearborn: 1947,
+    capital: 'isb',
+};
+console.log(country.yearborn);
---
+++ 21object.ts
@@ -0,0 +1,12 @@
+//regarding object
+
+
+let country = {
+yearborn: 1947,
+capital:'isb',
+
+}
+
+console.log(country.yearborn);
+
+
